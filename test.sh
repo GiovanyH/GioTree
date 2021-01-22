986c79c114eda942fc96730e516eb77209fcb21c
GiovanyH <henriquegiovany677@gmail.com>
2021-01-21 23:17:01 -0300

    sakodkoasd

--- test.sh
+++
@@ -1,4 +0,0 @@
-echo "Compilando Engine..."
-g++ -w -std=c++17 GioTree/src/Engine.cpp GioTree/src/log.cpp GioTree/src/core.cpp GioTree/src/window.cpp \
-	GioTree/src/projectManager.cpp -lSDL2 -o GioEngine
-echo "Done!"
